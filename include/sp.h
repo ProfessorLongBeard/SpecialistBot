9663c4d2b86e447b29b5a53804e8810060060132
ProfessorLongBeard <austingarrick752@gmail.com>
2024-09-22 15:45:28 -0700

    Update includes

--- include/sp.h
+++ include/sp.h
@@ -6,10 +6,12 @@
 #include <stdarg.h>
 #include <stdbool.h>
 
+#include <concord/log.h>
 #include <concord/discord.h>
 
 #include <events/on_ready.h>
 #include <events/on_sigint.h>
+#include <events/on_application_create.h>
 
 
 
